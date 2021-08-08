f0fbd7de2b8ed18a060ba0d7681cd672cb9a883f
adhyuva <adhyuva8@gmail.com>
2021-08-08 13:44:38 -0400

    More things

--- README.txt
+++ README.txt
@@ -23,3 +23,36 @@ then type these two lines of code:
 
 If you get some errors, do not worry. They are aviodable.
 (On VSCode I got two errors saying that it could not be imported. Don't worry!)
+
+Then type this: 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signUp.html')
+
+@app.route('/mainpage', methods=['POST'])
+def login():
+    return render_template('mainpage.html')
+
+@app.route('/createaccount', methods=['POST'])
+def createAccount():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        dateOfBirth = request.form['dateOfBirth']
+        hobby = request.form['hobby']
+        color = request.form['color']
+        print(email, password, dateOfBirth, hobby, color)
+        if email == '' or password ==  '' or  dateOfBirth =='' or hobby == '' or color == '':
+            return render_template('signUp.html', message = 'Please fill out <u>all</u> fields!')
+        return render_template('login.html', success = "You have succesfully made an account. Login to start the fun!")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
--- app.py
+++ app.py
@@ -1,7 +1,39 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
+
+ENV = 'dev'
+
+if ENV ==  'dev' :
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:database@localhost/personal-app'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class AccountInfo (db.Model):
+    __tablename__ = 'personal-app'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200), unique=True)
+    password = db.Column(db.String(200))
+    dateOfBirth = db.Column(db.DateTime())
+    favoriteColor = db.Column(db.String())
+    hobby = db.Column(db.String())
+
+    def __init__(self, email, password, dateOfBirth, favoriteColor, hobby):
+        self.email = email
+        self.password = password
+        self.dateOfBirth = dateOfBirth
+        self.favoriteColor = favoriteColor
+        self.hobby = hobby
+
+
 
 @app.route('/')
 def index():
@@ -13,21 +45,32 @@ def signup():
 
 @app.route('/mainpage', methods=['POST'])
 def login():
-    return render_template('mainpage.html')
+        email = request.form['email']
+        password = request.form['password']
+        accountInfo = AccountInfo.query.filter_by(email=email, password=password).first()
+        print(email,password)
+        if accountInfo:
+            print(accountInfo. f)
+        if email == '' or password == '':
+            return render_template('login.html', error = 'Please fill out <u>all</u> fields!')
+        return render_template('mainpage.html')
 
-@app.route('/createAccount', methods=['POST'])
+@app.route('/createaccount', methods=['POST'])
 def createAccount():
-    email = request.form['email']
-    password = request.form['password']
-    dateOfBirth = request.form['dateOfBirth']
-    hobby = request.form['hobby']
-    color = request.form['color']
-    print(email, password, dateOfBirth, hobby, color)
-    if email == '' or password ==  '' or  dateOfBirth =='' or hobby == '' or color == '':
-        return render_template('signUp.html', message = 'Uh - oh! Make sure to fill all the required fields')
-    return render_template('login.html')
-
+        email = request.form['email']
+        password = request.form['password']
+        dateOfBirth = request.form['dateOfBirth']
+        hobby = request.form['hobby']
+        color = request.form['color']
+        print(email, password, dateOfBirth, hobby, color)
+        if email == '' or password ==  '' or  dateOfBirth =='' or hobby == '' or color == '':
+            return render_template('signUp.html', message = 'Please fill out <u>all</u> fields!')
+        if db.session.query(AccountInfo).filter(AccountInfo.email == email).count()==0:
+            data = AccountInfo(email, password, dateOfBirth, color, hobby)
+            db.session.add(data)
+            db.session.commit()
+            return render_template('login.html', success = "You have succesfully made an account. Login to start the fun!")
+        return render_template('signUp.html', message = 'That account already exsists. Try using different information!!')
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
